e3ba3685061c3a4db0f8cb2b2c1bada8f718198a
FusionTech <125404931+fusiontechofficial@users.noreply.github.com>
2024-05-08 20:15:35 +0530

    first commit

---
+++ packages/uikit/src/components/Svg/Icons/LogoRound.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
+  return (
+    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+ width="1920.000000pt" height="513.000000pt" viewBox="0 0 1920.000000 513.000000"
+ preserveAspectRatio="xMidYMid meet">
+
+<g transform="translate(0.000000,513.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">
+</g>
+</svg>
+  );
+};
+
+export default Icon;
